41de1aa2ffe5e7dcb0000f18f0086ce8b6b57539
Federico Belvedere <fbelvedere@eaaf.org>
2024-05-08 20:24:20 -0300

    fix aspect ratio

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -4,10 +4,16 @@ import yop from '/src/assets/yop2.png'
 
 function Banner() {
     return (
-    <AspectRatio maxH={'20rem'} ratio={4 / 3}>
+    <AspectRatio 
+    maxH={'20rem'} 
+    ratio={16/9}
+    padding={0}
+    >
       <Box>
-        <video style={{ position: 'absolute', filter: 'brightness(40%)' }} loop muted autoPlay src={video} />
-        <Box position="absolute" padding={'3.5rem'}>
+        <video style={{ position: 'relative', filter: 'brightness(40%)' }} loop muted autoPlay src={video} />
+        <Box position="absolute" 
+        padding={'3.5rem'}
+        >
           <Center>
             <Image
               // position="absolute"
@@ -25,7 +31,7 @@ function Banner() {
             <Stack overflow={'hidden'}>
               <Heading color={'gray.100'} fontWeight={'bold'}>Who is Federico Belvedere?</Heading>
               <Text color={'gray.200'} fontSize={['sm', 'sm', 'md', 'xl']} pr='5rem'>
-                I am an <Highlight styles={{ px: '2', py: '1', rounded: 'full', bg: 'gray.100' }} query={['Information Systems Engineer', 'UTN', 'customer needs', 'delve into the code']}>Information Systems Engineer, graduated from the National Technology University (UTN), one of Argentina’s finest institutions.I have a passion for technology projects.My natural inclination is to be close to the business side, understanding processes and customer needs.However, my curiosity also drives me to delve into the code and comprehend what’s happening beneath the surface.Fortunately, I’m not just limited to that.Join me, and you’ll see what I mean.</Highlight></Text>
+                I am an <Highlight styles={{ px: '2', py: '1', rounded: 'full', bg: 'gray.100' }} query={['Information Systems Engineer', 'UTN', 'customer needs', 'delve into the code']}>Information Systems Engineer, graduated from the National Technology University (UTN), one of Argentina’s finest institutions.I have a passion for technology projects. My natural inclination is to be close to the business side, understanding processes and customer needs. However, my curiosity also drives me to delve into the code and comprehend what’s happening beneath the surface. Fortunately, I’m not just limited to that. Join me, and you’ll see what I mean.</Highlight></Text>
             </Stack>
           </Center>
         </Box>
